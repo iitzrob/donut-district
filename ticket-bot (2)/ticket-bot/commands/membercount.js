5eeafb1e7280d9b797288b562bc18395d19d0780
iitzrob <rizzbut3@gmail.com>
2026-09-21 00:43:38 +1000

    Update membercount.js

--- ticket-bot (2)/ticket-bot/commands/membercount.js
+++ ticket-bot (2)/ticket-bot/commands/membercount.js
@@ -1,26 +1,21 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+// Shown next to "Members". Change the id here to swap the emoji.
+const MEMBER_EMOJI = '<:Emojis_48x48_115:1551135209976762379>';
+
 const fmt = (n) => n.toLocaleString('en-US');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('membercount')
-    .setDescription('Show the member count and boost level of the server')
+    .setDescription('Show how many members the server has')
     .setDMPermission(false),
 
   async execute(interaction) {
-    const guild = interaction.guild;
-
     // memberCount counts everyone in the server, bots included.
     const description = [
-      '## Members',
-      '',
-      `* ***${fmt(guild.memberCount)}***`,
-      '',
-      '## Boosts',
-      '',
-      `* ***Level ${guild.premiumTier}***`,
-      `* ***${fmt(guild.premiumSubscriptionCount || 0)} Boosts***`,
+      `## ${MEMBER_EMOJI} Members`,
+      `* ***${fmt(interaction.guild.memberCount)}***`,
     ].join('\n');
 
     const embed = new EmbedBuilder().setColor(0x2b2d31).setDescription(description);
