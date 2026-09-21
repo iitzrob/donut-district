c5e5ffa94007bceb93b14185ad75b3df967754df
iitzrob <rizzbut3@gmail.com>
2026-09-21 16:01:54 +1000

    Create afk.js

---
+++ ticket-bot (2)/ticket-bot/commands/afk.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { setAfk } = require('../utils/afk');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('afk')
+    .setDescription('Set yourself as AFK')
+    .addStringOption((option) =>
+      option
+        .setName('reason')
+        .setDescription('Why you are AFK')
+        .setRequired(false)
+    )
+    .setDMPermission(false),
+
+  async execute(interaction) {
+    const reason = interaction.options.getString('reason') || 'AFK';
+
+    setAfk(interaction.user.id, reason);
+
+    const embed = new EmbedBuilder()
+      .setColor(0x2b2d31)
+      .setDescription(`✅ Set your status to **AFK**: ${reason}`);
+
+    return interaction.reply({ embeds: [embed] });
+  },
+};
